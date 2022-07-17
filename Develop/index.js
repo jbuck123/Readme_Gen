aab18ee68e8ea04fdbde24c3ef133ab19738001c
JamesB <James.buchmann@gmail.com>
2022-07-16 19:44:23 -0500

    creating an array of questions or prompts

--- Develop/index.js
+++ Develop/index.js
@@ -1,41 +1,51 @@
 // TODO: Include packages needed for this application
 const inquierer = require('inquirer');
 
-inquierer.prompt([
-    {
-    type: 'input',
-    name: 'age',
-    message: 'Please enter your age.'
-    // pass a question here that returns a promis 
-    }, // this is how u get multiple prompts 
-    {
-        type: "input",
-        name: 'user_name',
-        message: 'please enter your name.'
-    }, // another prompt
-    { 
-    type: 'list',
-    name: 'places',
-    message: 'please select your fav place from the list',
-    choices: ['minneapolis', 'Boston', 'LA']
-    },
-    {
-        type: 'checkbox',
-        name: 'choice',
-        messager: 'select your fave games',
-        choices: [ // this returns the value 
-            { name: 'call of duty', value: 'shooter' },
-            {name: 'minecraft', value: 'sandbox'},
-            { name: 'Runescape', value: 'MMO'}, 
-        ],
-    }
-]).then((data) => {
+inquierer.prompt(questions).then((data) => {
     console.log(data);
 
 });
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'Project Title',
+        message: " what is the title of the project?"
+    },
+    {
+        type: 'input',
+        name: "Description",
+        message: "what is the description of the project.?"
+    },
+    {
+        type: 'input',
+        name: "installation instructions",
+        message: "please enter installation instructions."
+    },
+    {
+        type: 'input',
+        name: "usage information",
+        message: "enter information about the applications usage."
+    },
+    {
+        type: 'input',
+        name: "contribution guidelines",
+        message: "what were the contribution guidelines?"
+    },
+    {
+       type: 'list',
+       name: "license",
+       message: "Choose a license",
+       choice: [
+           "license1", "license1", "license1", "license1", "license1", "license1", "license1"
+       ]
+    },
+    {
+
+    }
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
