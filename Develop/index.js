15fb5b1b52780f3e5703b243254bdcb3b407b6de
JamesB <James.buchmann@gmail.com>
2022-07-18 20:22:45 -0500

    finished need to polish things up

--- Develop/index.js
+++ Develop/index.js
@@ -37,10 +37,8 @@ const questions = [
        name: "license",
        message: "Choose a license",
        choices: [
-           "MIT", "IBM", "license1", "license1", "license1", "license1", "license1"
+           "MIT", "IBM", "ISC", "UNLICENSED"
        ],
-     // mit 
-       
     },
     {
         type: 'input',
@@ -57,40 +55,62 @@ const questions = [
 ];
 
 
-// function that writes 
-// inquierer.prompt(questions).then((answer) => {
-//     console.log(answer);
-    function writeToPage(answers) {
-        if(answers.license === 'MIT' ){
-            var badge  = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
-            var licenseText
-        }
-    }
+// function that writes to the read me
 
-    return`
+    function writeToPage(answer) {
+        if(answer.license === 'MIT' ){
+            var badge  = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+            var licenseText = 'Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:'
+        }
+        else if ( answer.license === 'IBM'){
+            var badge = '[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)'
+            var licenseText = 'Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:'
+        }
+        else if (answer.license === 'ISC'){
+            var badge = '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)'
+            var licenseText = 'Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:'
+        } else { 
+            var licenseText = 'UNLICENSED'
+            var badge = '';
+        }
+        console.log(licenseText, badge);
+    fs.writeFile('README.md',`
 # ${answer.title}
 ## Description
     ${answer.description}
-## Install Instructions 
+## Table of Contents
+- [installation](#installation)
+- [usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
     ${answer.install_instructions}
 ## Usage Information
     ${answer.usage_information}
 ## Contribution Guidelines
     ${answer.contribution_guidelines}
-### License
-    ${answer.license}
-    [![License: ${answer.license}](https://img.shields.io/badge/License-${answer.license}-yellow.svg)](https://opensource.org/licenses/${answer.license})
+### License ${answer.license}
+${badge}
+
+${licenseText}
 ## Contact me
     ${answer.github_username}
     ${answer.email}
-    `
-}
+    `, (err) =>{
+                if(err) {
+                    console.log(err);
+                }else {
+                    console.log("its working")
+                        }   
+                }
+ )};
 // TODO: Create a function to initialize app
 function init() {
     inquierer.prompt(questions).then((answer) => {
         writeToPage(answer)
     }
-)};
+)}
 
 // Function call to initialize app
 init();
